d888ebc2fb0fcb06a194e6b791ab09271de67d51
mingri31164 <3116430062@qq.com>
2025-02-18 21:58:01 +0800

    init

---
+++ admin/src/main/java/com/mingri/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,20 @@
+package com.mingri.shortlink.admin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: mingri31164
+ * @CreateTime: 2025/2/18 21:05
+ * @ClassName: ShortLinkAdminApplication
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+
+}
